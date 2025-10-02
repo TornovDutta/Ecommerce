d449c742c718f22b3da7bb9f28eec682ea12b67b
TornovDutta <tornovdutta@gmail.com>
2025-10-02 14:54:32 +0530

    add cart controller

--- src/main/java/org/example/ecommerce/controller/CartController.java
+++ src/main/java/org/example/ecommerce/controller/CartController.java
@@ -1,4 +1,9 @@
 package org.example.ecommerce.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cart")
 public class CartController {
 }
