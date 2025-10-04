fc5c33a4c292f014cd93bcde567f437171aa438a
TornovDutta <tornovdutta@gmail.com>
2025-10-04 19:44:54 +0530

    add Categories controller

--- src/main/java/org/example/ecommerce/controller/CategoriesController.java
+++ src/main/java/org/example/ecommerce/controller/CategoriesController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("product")
-public class ProductController {
+@RequestMapping("categories")
+public class CategoriesController {
 }
